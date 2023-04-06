5a9bf8793b88f8eb57812349b6a58e0e78c4a8cd
Manir Uddin <manir22sust@gmail.com>
2023-04-06 23:14:37 +0200

    Messages, orders and other pages

--- src/pages/messages/Messages.jsx
+++ src/pages/messages/Messages.jsx
@@ -1,5 +1,98 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Messages.scss";
+
 export const Messages = () => {
-  return <div>Messages</div>;
+  const message =
+    " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit reprehenderit fugit praesentium velit aspernatur accusantium veritatis provident debitis voluptatem nulla voluptas eveniet odio nisi, quis officiis cum sapiente. Et, ullam.";
+  return (
+    <div className="messages">
+      <div className="container">
+        <div className="title">
+          <h1>Messages </h1>
+        </div>
+
+        <table>
+          <tr>
+            <th>Buyer</th>
+            <th>Last Message</th>
+            <th>Date</th>
+            <th>Action</th>
+          </tr>
+          <tr className="active">
+            <td>John Doe</td>
+            <td>
+              {" "}
+              <Link to="/message/123" className="link">
+                {" "}
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td> 1 day ago </td>
+            <td>
+              <button>Mark as Read </button>
+            </td>
+          </tr>
+          <tr className="active">
+            <td>John Doe</td>
+            <td>
+              {" "}
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...{" "}
+              </Link>
+            </td>
+            <td> 1 day ago </td>
+            <td>
+              <button>Mark as Read </button>
+            </td>
+          </tr>{" "}
+          <tr>
+            <td>John Doe</td>
+            <td>
+              {" "}
+              <Link to="/message/123" className="link">
+                {" "}
+                {message.substring(0, 100)}...{" "}
+              </Link>
+            </td>
+            <td> 1 day ago </td>
+          </tr>{" "}
+          <tr>
+            <td>John Doe</td>
+            <td>
+              {" "}
+              <Link to="/message/123" className="link">
+                {" "}
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td> 1 day ago </td>
+          </tr>{" "}
+          <tr>
+            <td>John Doe</td>
+            <td>
+              {" "}
+              <Link to="/message/123" className="link">
+                {" "}
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td> 1 day ago </td>
+            <td></td>
+          </tr>{" "}
+          <tr>
+            <td>John Doe</td>
+            <td>
+              {" "}
+              <Link to="/message/123" className="link">
+                {" "}
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td> 1 day ago </td>
+          </tr>{" "}
+        </table>
+      </div>
+    </div>
+  );
 };
--- src/pages/myGigs/MyGigs.jsx
+++ src/pages/myGigs/MyGigs.jsx
@@ -1,5 +1,117 @@
 import React from "react";
 import "./MyGigs.scss";
+import { Link } from "react-router-dom";
 export const MyGigs = () => {
-  return <div>MyGirs</div>;
+  return (
+    <div className="myGigs">
+      <div className="container">
+        <div className="title">
+          <h1>Gigs </h1>
+          <Link to="/add" className="link">
+            <button> Add New gig</button>
+          </Link>
+        </div>
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Sales</th>
+            <th>Actions</th>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td> Gig 1</td>
+            <td> 88 </td>
+            <td>123 </td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td> Gig 1</td>
+            <td> 88 </td>
+            <td>123 </td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td> Gig 1</td>
+            <td> 88 </td>
+            <td>123 </td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td> Gig 1</td>
+            <td> 88 </td>
+            <td>123 </td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td> Gig 1</td>
+            <td> 88 </td>
+            <td>123 </td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td> Gig 1</td>
+            <td> 88 </td>
+            <td>123 </td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
 };
