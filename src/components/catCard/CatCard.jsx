a5f60a8e547619c6207ee68eecc984c83ad32fef
Manir Uddin <manir22sust@gmail.com>
2023-04-08 02:09:01 +0200

    updated

--- src/components/catCard/CatCard.jsx
+++ src/components/catCard/CatCard.jsx
@@ -3,7 +3,7 @@ import "./CatCard.scss";
 import { Link } from "react-router-dom";
 export const CatCard = ({ item }) => {
   return (
-    <Link to="/gig?cat=design">
+    <Link to="/gigs">
       <div className="catCard">
         <img src={item.img} alt="" />
         <span className="desc"> {item.desc} </span>
